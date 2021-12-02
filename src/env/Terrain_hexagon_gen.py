3820fbf1ad6e67100ee0cbe4b03c594c444ad630
Le Ogu <hugo.coptbignon@gmail.com>
2021-12-02 18:44:23 +0100

    Update Terrain_hexagon_gen.py
    j'ai mis des shaders randoms et le pas random du displacement ne marche pas

--- src/env/Terrain_hexagon_gen.py
+++ src/env/Terrain_hexagon_gen.py
@@ -4,8 +4,8 @@ cmds.file(f=True, new=True)
 cpcurve = 0
 
 #valeur à changer et mettre dans une fenetre
-arrayZ = 30 #nombre sur l'axe des z
-arrayX = 30 #nombre sur l'axe des x
+arrayZ = 10 #nombre sur l'axe des z
+arrayX = 6 #nombre sur l'axe des x
 min = 0 #minumim du displacement
 max = 1 #maximum du displacement
 
@@ -20,25 +20,60 @@ for x in range (0,arrayX,1) :
         cpcurve = cpcurve+arrayX
 
 #creation du displacement
-
-for dis in range (0,arrayZ*arrayX,rpas) :
-    rpas = random.randint(1,5)
+rpas = random.randint(1,5)
+for dis in range (0,arrayZ*arrayX,rpas) :    
     for a in range (0,2,1) :
         fY = random.randint(min,max)
         cmds.select("curve"+str(dis+1)+".cv["+str(a)+"]")
         cmds.softSelect(sse=True)
         cmds.move(0,fY,0, relative=True)
-        """cmds.softSelect(sse=False)"""
+        cmds.softSelect(sse=False)
+        
+#creation des shaders
+myShader1 = cmds.shadingNode('blinn', asShader=True)
+cmds.setAttr('blinn1.colorR',1)
+cmds.setAttr('blinn1.colorG',0)
+cmds.setAttr('blinn1.colorB',0)
+cmds.sets(renderable=True, noSurfaceShader=True, empty=True, name='blinn1SG')
+cmds.connectAttr('blinn1.outColor','blinn1SG.surfaceShader')
+
+myShader2 = cmds.shadingNode('blinn', asShader=True)
+cmds.setAttr('blinn2.colorR',0)
+cmds.setAttr('blinn2.colorG',1)
+cmds.setAttr('blinn2.colorB',0)
+cmds.sets(renderable=True, noSurfaceShader=True, empty=True, name='blinn1SG')
+cmds.connectAttr('blinn2.outColor','blinn1SG1.surfaceShader')
+
+myShader3 = cmds.shadingNode('blinn', asShader=True)
+cmds.setAttr('blinn3.colorR',0)
+cmds.setAttr('blinn3.colorG',0)
+cmds.setAttr('blinn3.colorB',1)
+cmds.sets(renderable=True, noSurfaceShader=True, empty=True, name='blinn1SG')
+cmds.connectAttr('blinn3.outColor','blinn1SG2.surfaceShader')
+
+#creation des fonctions pour assigner les shaders random
+def blcolor1() :
+    cmds.sets(edit=True, forceElement= 'blinn1SG')    
+
+def blcolor2() :
+    cmds.sets(edit=True, forceElement= 'blinn1SG1')
+
+def blcolor3() :
+    cmds.sets(edit=True, forceElement= 'blinn1SG2')
 
 #creation des cylindres selon les lignes
 for nb in range (0,arrayZ*arrayX,1) :
-    cmds.softSelect(sse=False)
     for c in range (0,2,1) :
         posV = cmds.xform("curve"+str(nb+1)+".cv["+str(c)+"]", ws = True, q=True,t=True)
         posX = posV[0]
         posY = posV[1]
         posZ = posV[2]
         cmds.polyCylinder(sx=6)
+        #assigne des shaders random
+        my_list = [(blcolor1),(blcolor2),(blcolor3)]
+        my_samp = 1
+        numColor = random.sample(my_list,my_samp)
+        numColor[0]()
         #bevel des edges du dessus
         cmds.polyBevel("pCylinder"+str((c+1)+((nb)*2))+".e[7]","pCylinder"+str((c+1)+((nb)*2))+".e[8]","pCylinder"+str((c+1)+((nb)*2))+".e[9]","pCylinder"+str((c+1)+((nb)*2))+".e[10]","pCylinder"+str((c+1)+((nb)*2))+".e[11]","pCylinder"+str((c+1)+((nb)*2))+".e[6]",offset=0.1)
         cmds.scale(1.15,1.15,1.15)
