03164344bb8d20a2def27fe3f207d7b6cd4ed6a7
Le Ogu <hugo.coptbignon@gmail.com>
2021-12-02 10:57:07 +0100

    Update Terrain_hexagon_gen.py
    J'ai fais des modifs sur l'ordre pour mieux gerer le displacement

--- src/env/Terrain_hexagon_gen.py
+++ src/env/Terrain_hexagon_gen.py
@@ -3,8 +3,10 @@ import random as random
 cmds.file(f=True, new=True)
 
 arrayZ = 10
-arrayX = 10
+arrayX = 5
 cpcurve = 0
+min = 0
+max = 1
 
 for x in range (0,arrayX,1) :
     cmds.select( all=True )
@@ -13,16 +15,23 @@ for x in range (0,arrayX,1) :
     for z in range (0,arrayZ,1) :
         cmds.curve( p=[(-0.862225, 0, -0.5), (0.862225, 0, 0.5)])
         cmds.move(0,0,z*2)
-        for c in range (0,2,1) :
-            Rhigh = random.uniform(0,1)
-            Rhigh2 = random.uniform(0,1)
-            posV = cmds.xform("curve"+str((z+1)+cpcurve)+".cv["+str(c)+"]", ws = True, q=True,t=True)
-            print "curve"+str((z+1)+cpcurve)
-            posX = posV[0]
-            posY = posV[1]
-            posZ = posV[2]
-            cmds.polyCylinder(sx=6)
-            cmds.scale(1.15,1.15,1.15)
-            cmds.move(posX,posY+Rhigh+Rhigh2,posZ)
-    cpcurve = cpcurve+arrayX
+        cpcurve = cpcurve+arrayX
+
+rpas = random.randint(1,5)
+for dis in range (0,arrayZ*arrayX,rpas) :
+    for a in range (0,2,1) :
+        fY = random.randint(min,max)
+        cmds.select("curve"+str(dis+1)+".cv["+str(a)+"]")
+        cmds.move(0,fY,0, relative=True)
+
+
+for nb in range (0,arrayZ*arrayX,1) :
+    for c in range (0,2,1) :
+        posV = cmds.xform("curve"+str(nb+1)+".cv["+str(c)+"]", ws = True, q=True,t=True)
+        posX = posV[0]
+        posY = posV[1]
+        posZ = posV[2]
+        cmds.polyCylinder(sx=6)
+        cmds.scale(1.15,1.15,1.15)
+        cmds.move(posX,posY,posZ)
 cmds.select( all=True )
