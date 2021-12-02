81b46c2f75cbac5a9e882fa9da4a101863a6783f
Le Ogu <hugo.coptbignon@gmail.com>
2021-12-02 12:04:00 +0100

    Update Terrain_hexagon_gen.py
    j'ai clean le code avec des indications sur les boucles

--- src/env/Terrain_hexagon_gen.py
+++ src/env/Terrain_hexagon_gen.py
@@ -1,13 +1,15 @@
 import maya.cmds as cmds
 import random as random
 cmds.file(f=True, new=True)
-
-arrayZ = 10
-arrayX = 10
 cpcurve = 0
-min = 0
-max = 1
 
+#valeur à changer et mettre dans une fenetre
+arrayZ = 30 #nombre sur l'axe des z
+arrayX = 30 #nombre sur l'axe des x
+min = 0 #minumim du displacement
+max = 1 #maximum du displacement
+
+#creation des lignes 
 for x in range (0,arrayX,1) :
     cmds.select( all=True )
     cmds.move(3.45,0,0, relative=True)
@@ -17,6 +19,7 @@ for x in range (0,arrayX,1) :
         cmds.move(0,0,z*2)
         cpcurve = cpcurve+arrayX
 
+#creation du displacement
 rpas = random.randint(1,5)
 for dis in range (0,arrayZ*arrayX,rpas) :
     for a in range (0,2,1) :
@@ -26,7 +29,7 @@ for dis in range (0,arrayZ*arrayX,rpas) :
         cmds.move(0,fY,0, relative=True)
         """cmds.softSelect(sse=False)"""
 
-
+#creation des cylindres selon les lignes
 for nb in range (0,arrayZ*arrayX,1) :
     cmds.softSelect(sse=False)
     for c in range (0,2,1) :
@@ -36,6 +39,7 @@ for nb in range (0,arrayZ*arrayX,1) :
         posZ = posV[2]
         cmds.polyCylinder(sx=6)
         print "pCylinder"+str((c+1)+((nb)*2))
+        #bevel des edges du dessus
         cmds.polyBevel("pCylinder"+str((c+1)+((nb)*2))+".e[7]","pCylinder"+str((c+1)+((nb)*2))+".e[8]","pCylinder"+str((c+1)+((nb)*2))+".e[9]","pCylinder"+str((c+1)+((nb)*2))+".e[10]","pCylinder"+str((c+1)+((nb)*2))+".e[11]","pCylinder"+str((c+1)+((nb)*2))+".e[6]",offset=0.1)
         cmds.scale(1.15,1.15,1.15)
         cmds.move(posX,posY,posZ)
