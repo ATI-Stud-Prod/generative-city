76b87488b660cbd7049dd0306453aae1fdacfe95
Gilles Avraam <gillouavraam@gmail.com>
2021-12-06 14:31:06 +0100

    update, import okkk, terrain + route

---
+++ src/env/route.py
@@ -0,0 +1,49 @@
+import math
+import maya.cmds as cmds
+import random as random
+import mtoa.utils as mutils
+cmds.file(f=True,new=True)
+
+#-----------------------------------------------------
+
+def Scene():
+    
+    cmds.file(f=True,new=True)
+    
+
+
+# A-Création d'un shader et application du shader de base
+
+def shaderRoute():
+    
+    cmds.file(f=True,new=True)
+
+b = cmds.shadingNode('lambert', asShader=True)
+cmds.sets(renderable=True, noSurfaceShader=True, empty=True, name='lambert1SG')
+cmds.connectAttr('lambert1.outColor', 'lambert1SG.surfaceShader')
+
+#ajout texture
+
+cmds.shadingNode('file', asTexture=True)
+cmds.shadingNode('place2dTexture', asUtility=True)
+cmds.connectAttr( 'place2dTexture1.outUV', 'file1.uvCoord')
+cmds.connectAttr('file1.outColor', 'lambert1.color')
+
+
+r = random.randint(mini,maxi)
+
+mini = 1
+maxi = 40
+
+
+def Route():
+    
+    cmds.file(f=True,new=True)
+
+for p in range(r):
+    sphereName = cmds.polyPlane(name = "route"+str(1))
+    sphereName = "pPlane"+str(p+1)
+    cmds.sets(edit=True, forceElement = 'lambert1SG')
+    posX = p
+    posZ = 0
+    cmds.move(posX,posZ,xz=True)
--- src/env/water.py
+++ src/env/water.py
@@ -1,63 +1,9 @@
 import math
 import maya.cmds as cmds
 import random as random
-import mtoa.utils as mutils
-from functools import partial
-
-cmds.file(f=True, new=True)
-
-def world():
-    mutils.createLocator("aiSkyDomeLight", asLight=True)
-    panel = cmds.getPanel(wf=1)
-    cmds.modelEditor(panel, e=1, allObjects=0)
-    cmds.modelEditor(panel, e=1, polymeshes=1) 
-    cmds.modelEditor(panel, e=1, displayTextures=1)
 
 
-def getSliderValue(ctrlName):
-    value = cmds.intSlider(ctrlName, q=True, value=True)
-    return value
-
-def moveXYZ(slider, *arg, **kwargs):
-    value = getSliderValue(slider)
-    #cmds.duplicate(n=object,st=False)
-    cmds.select()
-    cmds.move(value,*arg, **kwargs)
-
-def Pre():
-    print("test")
-class Env:
-    def __init__(self,water, sizeX, sizeY):
-        self.water = water
-        self.sizeX = sizeX
-        self.sizeY = sizeY
-
-    def UI(self="UI"):
-        window = cmds.window( title="Long Name", iconName='Short Name', widthHeight=(200, 800), s = False, menuBar = True)
-        cmds.columnLayout( adjustableColumn=True)
-        mySlider = cmds.intSlider(min=1, max=100, value=0, step=1, dc = 'empty')
-        #cmds.intSlider(mySlider, e=True, dc = partial(moveXYZ, mySlider, x=1))
-        slider1 = cmds.floatSliderGrp(min =0.0,max=1.0,label="BlinnRed", field= True, backgroundColor=[1,1,1])
-        
-        #cmds.setAttr(O_shader+'.scale',5)
-        # cmds.text(label="City Width", al = "left")
-        # cityWidth = cmds.textField( tx = "700")
-        # progressControl = cmds.progressBar(maxValue=100, width=100, vis = False)
-        
-        #cmds.button(label = "Generate", c = partial(cityWidth))
-        
-        #show window
-        cmds.showWindow(window)
-
-    def sqSetClustersZeroScale(self, *arqs):
-        if self.clusterList:
-            for item in self.clusterList:
-                cmds.setAttr(item+".scaleX", 0)
-                cmds.setAttr(item+".scaleY", 0)
-                cmds.setAttr(item+".scaleZ", 0)
-
-
-    def water(self=" "):
+def water():
         object = cmds.polyPlane(height=10, width=10)
         print(object)
         O_shader = cmds.shadingNode('oceanShader', asShader=True)
@@ -74,7 +20,5 @@ class Env:
         cmds.sets(edit=True, forceElement=O_shader+'SG')
         cmds.setAttr(O_shader+'.scale',5)
         
-    
+        
 
-Env.water()
-Env.UI()
