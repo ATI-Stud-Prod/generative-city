4c32986f0bc095b6eb7ffa4db96b8977647ca3a6
gilles avraam <49715824+Avgilles@users.noreply.github.com>
2021-12-06 14:39:35 +0100

    Terrain 1 (#25)
    * Create example.py
    
    * Update water.py
    
    * for pull request
    
    * feat: terrain-WIP
    
    Creation d'un terrain avec une base qui a plusieurs hexagone ou qui permet de creer un terrain traditionel
    
    * feat:terrain (WIP)
    
    deplacement de terrain dans environnement
    
    * update develop
    
    * Create Terrain_hexagon_gen.py
    
    bon ca fait des hexagon random sur une grille, mais je referrai pour pouvoir faire des beaux terrains
    
    * Update Terrain_hexagon_gen.py
    
    J'ai fais des modifs sur l'ordre pour mieux gerer le displacement
    
    * Update Terrain_hexagon_gen.py
    
    j'ai tout fais smooth et j'ai mi un bevel que sur les face du dessus
    
    * Update Terrain_hexagon_gen.py
    
    j'ai clean le code avec des indications sur les boucles
    
    * Update Terrain_hexagon_gen.py
    
    j'ai changé de place le raps du displacement pour pas voir le patern
    
    * Update Terrain_hexagon_gen.py
    
    j'ai mis des shaders randoms et le pas random du displacement ne marche pas
    
    * Update Terrain_hexagon_gen.py
    
    mini correction des couleurs
    
    * Update Terrain_hexagon_gen.py
    
    j'ai mis les textures selon le terrain
    
    * Update Terrain_hexagon_gen.py
    
    des minis corrections sur le random et le code
    
    * Update Terrain_hexagon_gen.py
    
    essaie de correction de l'alléatoire
    
    * Update Terrain_hexagon_gen.py
    
    correctif
    
    * update, import okkk, terrain + route
    
    Co-authored-by: Manu Cheremeh <manucheremeh1995@gmail.com>
    Co-authored-by: Le Ogu <hugo.coptbignon@gmail.com>

---
+++ src/env/water.py
@@ -0,0 +1,24 @@
+import math
+import maya.cmds as cmds
+import random as random
+
+
+def water():
+        object = cmds.polyPlane(height=10, width=10)
+        print(object)
+        O_shader = cmds.shadingNode('oceanShader', asShader=True)
+        bs = cmds.sets(renderable=True, noSurfaceShader=True, empty=True, name='oceanShader1SG')
+        cmds.connectAttr(O_shader + '.outColor', bs + '.surfaceShader')
+        cmds.connectAttr(O_shader + '.outColor', bs + '.displacementShader')
+        O_shader2 = cmds.shadingNode('blinn', asRendering=True)
+        cmds.connectAttr(O_shader + '.outColor', O_shader2 + '.transparency')
+
+        # Régler la couleur du shader
+        print(O_shader)
+        cmds.select(str(object[0]))
+    
+        cmds.sets(edit=True, forceElement=O_shader+'SG')
+        cmds.setAttr(O_shader+'.scale',5)
+        
+        
+
