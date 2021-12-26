bcd7f9ea1c50ca8453afa8cf6fdc21a1fbfb0514
Gilles Avraam <gillouavraam@gmail.com>
2021-12-26 18:19:15 +0100

    add subdivid and detail

--- src/__main__.py
+++ src/__main__.py
@@ -5,19 +5,6 @@ def importMayaScript(nameFolder):
 
     myScriptDir = cmds.internalVar(userScriptDir=True)
     setScriptDir = myScriptDir+'generative-city/src/'+str(nameFolder)+'/'
-    
-    #if(sys.path == "/generative-city/src/env/"):
-    """
-    pathExist = False;
-    for i in range(len(sys.path)-1):
-        
-        if(sys.path[i] == setScriptDir):
-            pathExist = True
-            
-    if(pathExist == True):
-        print("test")
-        sys.path.append(setScriptDir)
-    """
     sys.path.append(setScriptDir)
     
 importMayaScript("env")      
--- src/element/batt.py
+++ src/element/batt.py
@@ -32,97 +32,107 @@ def selectEdgeArround(theFace):
 
         
 # ref : https://www.pinterest.fr/pin/93801604729453729/
+""" @buildingType = [basic, elongated]"""
+
 class Element:
-    def __init__(self, hauteur, largeur,sub, typeTexture='null'):
+    #self.mesh = cmds.polyCylinder(name="baseMesh", sx=self.sub,sy=1, sz=1, h=self.hauteur,radius=self.largeur)
+
+    
+    def __init__(self, hauteur, largeur,sub, buildingType="basic",  translateZ=0, typeTexture='null'):
         self.hauteur = hauteur
         self.largeur = largeur
+        self.buildingType = buildingType
         self.sub = sub
-        self.typeTexture = typeTexture
-
-    def building(self, nbEtage):
-        for i in range(nbEtage):
-            self.floor()
-            cmds.move(0,(i)*self.hauteur,0, moveXZ=False)
         
-    
-    def floor(self):
-        print("start create floor")
-        mesh = cmds.polyCylinder(name="baseMesh", sx=self.sub,sy=1, sz=1, h=self.hauteur,radius=self.largeur)
+        self.typeTexture = typeTexture
+        
+        self.translateZ = translateZ 
 
-        """ Set up of our variable and base mesh """
+
+    def structureMesh(self,PosX, PosY, PosZ, Rotation=0 , wallSubX=0,wallSubY=0 ):
+        mesh = cmds.polyCylinder(name="baseMesh", sx=self.sub,sy=1, sz=1, h=self.hauteur,radius=self.largeur)
         if (self.sub == 4 ):
             cmds.delete(mesh[0]+'.f[8:10]')
             cmds.delete(mesh[0]+'.f[4:8]')
-            cmds.rotate(0, '45deg',0)
-            cmds.move(0,self.hauteur*.5,0)
             cmds.polyCloseBorder(ch=True)
+            
+            
+            cmds.delete(mesh[0]+'.f[0:'+str((self.sub*2)-1)+']')
+            cmds.select(mesh[0])
+            cmds.move(0,-self.hauteur*.5,0, moveXZ=False)
+            cmds.rotate(0,'45deg',0)
             cmds.delete(mesh[0], constructionHistory = True)
             cmds.makeIdentity(apply=True, t=1, r=1, s=1, n=0)
-
-            MeshValue = cmds.polyEvaluate()
-            lastVertex = int((MeshValue['vertex'] /2) -1)
-            lastEdge = int((MeshValue['edge'] /2) -1)
-            lastFace = int((MeshValue['face'] /2) -1)
             
-            selectTopFace = mesh[0]+'.f['+str(lastFace)+']'
-            # Start mode 
-            cmds.polyBevel(mesh[0]+'.f['+str(lastFace)+']',segments=1, autoFit=False )
-
-
+            
         else:
-            cmds.move(0,self.hauteur*.5,0)
-            cmds.delete(mesh[0], constructionHistory = True)
-            cmds.makeIdentity(apply=True, t=1, r=1, s=1, n=0)
-            MeshValue = cmds.polyEvaluate()
-            lastVertex = MeshValue['vertex'] -1
-            lastEdge = MeshValue['edge'] -1
-            lastFace = MeshValue['face'] -1
+            cmds.move(PosX, PosY-self.hauteur*.5, PosZ)
+            cmds.delete(mesh[0]+'.f[0:'+str((self.sub*2)-1)+']')
+        
+
+        cmds.rotate(0,''+str(Rotation)+'deg',0)
+        cmds.polySubdivideFacet(dv=wallSubX)
+        cmds.select(mesh[0]+'.f[0:*]')
+        cmds.polyExtrudeFacet(kft=True, ltz=self.hauteur, divisions=wallSubY)
+        cmds.delete(mesh[0], constructionHistory = True)
+        cmds.makeIdentity(apply=True, t=1, r=1, s=1, n=0)
+
+        MeshValue = cmds.polyEvaluate()
+        lastVertex = MeshValue['vertex'] -1
+        lastEdge = MeshValue['edge'] -1
+        lastFace = MeshValue['face'] -1
            
-            selectTopFace = mesh[0]+'.f['+str(lastFace-self.sub+1)+':'+str(lastFace)+']'
+        selectTopFace = mesh[0]+'.f['+str(lastFace-self.sub+1)+':'+str(lastFace)+']'
+        DataMesh = {"meshName":mesh[0], "lastVertex":lastVertex, "lastEdge":lastEdge, "lastEdge": lastFace, "selectTopFace" : selectTopFace}
+        
+        return(DataMesh)
 
-            # Start mode           
-            cmds.polySelect(edgeLoop=[5])
-            #cmds.polySelect(edgeLoop=[1], add=True)
-            cmds.polyBevel(segments=1, autoFit=False)
-            # cmds.polySplit(constructionHistory=True, smoothingangle=[180],edgepoint=[5, .5,[baseMesh,baseMeshShape])
-            
-            # cmds.hilite(loopSelection[0],loopSelection[1])
-            # cmds.polySplit()
+    def building(self, nbEtage):
+        createGroupe = cmds.group( em=True, name='building_'+str(self.buildingType) )
+        listforGroup = []
+        for i in range(nbEtage):
+            self.floor()
+            cmds.move(0,(i)*(self.hauteur+self.translateZ),0, moveXZ=False)
+    
+    def elongated(self, division, offset, keepFacesTogether):
+        cmds.polyExtrudeFacet(ltz=self.translateZ, d=division, off=offset, kft=keepFacesTogether)
+    
+    def floor(self):
+        print("start create floor")
 
+        mesh = self.structureMesh(1,1,1)
+        if (self.buildingType == "elongated"):
+            cmds.select(selectTopFace)
+            self.elongated(3,1.5, True)
+            cmds.polyBevel()
+            #cmds.polyExtrudeFacet(kft=False, ltz=2)
+            #cmds.select(mesh[0]+'.f['+str(lastFace)+']', tgl=True)
+        else:
+            cmds.polyBevel()
         
-         
-   
-        #selectEdgeArround(selectTopFace)
-        """
-        cmds.polyExtrudeFacet(mesh[0]+'.f['+str(lastFace)+']', kft=False, ltz=2)
-        cmds.select(mesh[0]+'.f['+str(lastFace)+']', tgl=True)
-        """
-
-        #cmds.polyExtrudeFacet(mesh+'.f['+str(lastFace)+']', kft=False, ltz=2)
-        #print(lastEdge)
-        
-        
-        #cut1 = cmds.polyCut(cd='x', ef=0, ch=1 )
-        #cmds.select(mesh[0]+'.e['+str(lastEdge+self.sub)+':'+str(lastEdge+(self.sub)*2)+']')
-        
-        #cmds.select( cut1, tgl=True )
-        #cmds.polyBevel(mesh[0]+'.e['+str(lastEdge+1)+":"+str(lastEdge+self.sub)+']',segments=4, autoFit=False )
-
-        # cmds.select(test)
-        
-        #cmds.polyCut(cd='Z', ef=0, ch=1 )
-        
+        """ A activer"""
         cmds.select(cl=True)
-        cmds.select(mesh[0])
+        cmds.select(mesh["meshName"])
+        
+    
+    
+    def Test(self):
+        test= self.structureMesh(1,1,1)
+        print(test)
+    
 
 if __name__ == '__main__':
     
     """---INTERFACE---"""
-    
+    """ 
     ui = UI('interface',300, 200)
     ui.make_btn(label='buton')
     ui.make_i_SliderGrp()
-    
+    """
     """---Build---"""
-    init = Element(3,5,5)
-    init.building(12)
+    init = Element(3,5,4, "basic")
+    init.building(10)
+    #init.structureMesh(0,0,0)
+    #init.Test()
+
+    
