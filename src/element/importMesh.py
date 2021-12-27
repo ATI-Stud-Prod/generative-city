d73ff2993171cac65e0280a095d3ebb0fd8a51b9
Gilles Avraam <gillouavraam@gmail.com>
2021-12-27 04:02:21 +0100

    Update importMesh.py
    Création d'une class import correct

--- src/element/importMesh.py
+++ src/element/importMesh.py
@@ -6,19 +6,56 @@ cmds.file(f=True, new=True)
 myScriptDir = cmds.internalVar(userScriptDir=True)
 setMeshDir = myScriptDir+'generative-city/src/element/mesh/'
 
-def openMayaFile(NameFile):
-    # cmds.file( setMeshDir+NameFile, o=True )
-    cmds.file( setMeshDir+NameFile, r=True)
-    #cmds.file( setMeshDir+NameFile, r=True, type='mayaAscii', namespace='rubble' )
 
-    # Query the setAttr edits:
-    #cmds.reference( rfn='batiment_CyberPunk_uv', q=True, editCommand=True )
+class ManageImport:
+    
+    def __init__(self,NameFile,path="" ,SelectedObj="", referenceName=""):
+        self.NameFile = NameFile
+        self.path = path
+        self.SelectedObj = SelectedObj
+        self.referenceName = referenceName
+        
+    def openMayaFile(self,referenceName="" ):
+        if(referenceName != ""):
+             Mayafile = cmds.file( self.path, removeReference=True)
+             self.NameFile = referenceName
+        
+        self.path= setMeshDir+self.NameFile
+        Mayafile = cmds.file( self.path, r=True, lockFile=False)
+        nameFile = cmds.referenceQuery( self.path,  nodes=True  ) # bad
+        self.SelectedObj = nameFile[0]
+        self.referenceName = cmds.referenceQuery(self.path, filename = True)
+
+        
+    def changeReference(self, nameReference=""):
+        cmds.file( self.path, unloadReference=nameReference)
+        openMayaFile()
+    
+    def ManageObj(self,_pos=[0,0,0], _scale=[1,1,1], _rotation=["0deg","0deg","0deg"]):
+        obj = self.SelectedObj
+        cmds.select(obj)
+        cmds.move(_pos[0],_pos[1],_pos[2])
+        cmds.scale(_scale[0],_scale[1], _scale[2])
+        cmds.rotate(str(_rotation[0])+"deg",str(_rotation[1])+"deg",str(_rotation[2])+"deg")
+
+        
+    
+if __name__ == '__main__':
+    bilelBat = ManageImport("batiment_CyberPunk_uv.mb")
+    bilelBat.openMayaFile()
+    bilelBat.ManageObj([0,0,0],[.5,.5,.5], [45,0,0])
+    
+    """ Si vous voulez changer après coup la reference vous pouvez spécifier un autre fichier """
+    # bilelBat.openMayaFile("batiment_CyberPunk_avant_combine.mb")
+
+"""------------- Texture part ---------------"""
 
 def openTextureFile(nameFile):
     filePath = str(setMeshDir) + str(nameFile)
     return(filePath)
 
-openMayaFile("batiment_CyberPunk_uv.mb")
+
+#deleteMayaFile("batiment_CyberPunk_uv")
 
 """
 b = cmds.shadingNode('lambert', asShader=True)
